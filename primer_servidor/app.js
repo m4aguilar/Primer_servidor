2032996482c901f76cc68b70bc2ae2ce1b1ed45a
m4aguilar <mario4.1990@gmail.com>
2016-11-22 17:19:17 +0100

    22-11-16 v2
    2.0

--- primer_servidor/app.js
+++ primer_servidor/app.js
@@ -1,7 +1,6 @@
 var express = require('express');
 var mongoose = require('mongoose');
-var path = require("path");
-
+var bodyParser = require('body-parser');
 
 var app = express();
 
@@ -11,6 +10,11 @@ var app = express();
 //mongodb://localhost/"nombre_base_datos"
 //mongoose.Promise = global.Promise;
 mongoose.connect("mongodb://localhost/primer_servidor");
+
+//Para decirle a express que se va a usar body parser
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+
 
 //Definimos el schema del login
 var login = {name:String, pass:String};
@@ -30,19 +34,31 @@ app.use(express.static("public"));
 
 //req: solicitud, res: respuesta
 app.get("/", function(req, res){
-
-
     //render sirve para renderizar una vista
     //La extension no hay que ponersela porque ya
     //indicamos que era una tipo "jade"
-
-    var product = new Product(data);
-    product.save();
-
     res.render("index");
 });
+
+app.post("/menu", function(req, res){
+  if(req.body.password == "1234"){
+    var data = {
+        title: req.body.title,
+        description: req.body.description,
+        imageUrl: "hola.png",
+        price: req.body.price
+    }
+    var product = new Product(data);
+    console.log(req.body);
+    product.save();
+    res.render("index");
+  }else {
+    res.render("menu/new");
+  }
+});
+
 app.get("/menu/new",function(req, res){
-  res.render("menu/new")
+  res.render("menu/new");
 });
 
 app.listen(8080);
