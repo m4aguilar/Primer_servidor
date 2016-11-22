35567f88b57f6de780d1f9655125389600ff418d
m4aguilar <mario4.1990@gmail.com>
2016-11-22 10:42:04 +0100

    nuevos cambios
    mas cambios

--- primer_servidor/app.js
+++ primer_servidor/app.js
@@ -15,9 +15,9 @@ mongoose.connect("mongodb://localhost/primer_servidor");
 //Definimos el schema del login
 var login = {name:String, pass:String};
 var productSchema = {
-    title:String, 
+    title:String,
     description:String,
-    imageUrl:String, 
+    imageUrl:String,
     price:Number
 };
 
@@ -33,24 +33,16 @@ app.get("/", function(req, res){
 
 
     //render sirve para renderizar una vista
-    //La extension no hay que ponersela porque ya 
+    //La extension no hay que ponersela porque ya
     //indicamos que era una tipo "jade"
-    
-
-    var data = {
-        title: "Mi primer producto",
-        description:"Descripcion fisica",
-        imageUrl:"No hay na",
-        price: 2
-    }
 
     var product = new Product(data);
     product.save();
-    //product.save(function(err){
-    //    console.log(product);
-    //});
 
     res.render("index");
+});
+app.get("/menu/new",function(req, res){
+  res.render("menu/new")
 });
 
 app.listen(8080);
